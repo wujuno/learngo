7a5d145dd7074b393ce905a92acc0e03b16da59b
wujuno <huj88854@gmail.com>
2023-06-11 01:23:14 +0900

    업데이트, 삭제 메서드 추가

--- main.go
+++ main.go
@@ -9,16 +9,12 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{}
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	err :=dictionary.Delete("wf3fdsf")
 	if err != nil {
 		fmt.Println(err)
-	}
-	hello, _ := dictionary.Search(word)
-	fmt.Println("found", word, "definition:", hello)
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
+	} else {
+		fmt.Println(baseWord,"is deleted")
 	}
 }
