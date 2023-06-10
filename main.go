03f46e19d4654c3e36b16f5de6fa66b060ad1590
wujuno <huj88854@gmail.com>
2023-06-11 01:04:10 +0900

    maps on Go are automatically using *

--- main.go
+++ main.go
@@ -8,11 +8,17 @@ import (
 
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("second")
+	dictionary := mydict.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+	}
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found", word, "definition:", hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
 	}
 }
