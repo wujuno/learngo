859444379dd6c2a1886e14232b289d6c92974494
wujuno <huj88854@gmail.com>
2023-06-11 01:45:05 +0900

    http 라이브러리

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/wujuno/learngo/mydict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	err :=dictionary.Delete("wf3fdsf")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(baseWord,"is deleted")
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
